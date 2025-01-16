e0f167fced88b1d99f620338aecc432aa3a324f0
Fouzia ifat <fouzia786ifat@gmail.com>
2025-01-16 17:12:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// https://preview.themeforest.net/item/raz-clean-minimal-woocommerce-theme/full_screen_preview/23770232
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+
+  // Toggle icon between menu and close icons
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
